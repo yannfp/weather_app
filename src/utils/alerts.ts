f25d9a4f51a712c8a36334ee4fb5cfb1e371bd92
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-03-08 10:53:31 +0100

    add: temperature unit conversion

--- src/utils/alerts.ts
+++
@@ -1,9 +0,0 @@
-import { Alert } from "react-native";
-
-export function showAlert(title: string, message: string) {
-    if (typeof window !== "undefined" && window.alert) {
-        window.alert(`${title}\n\n${message}`);
-    } else {
-        Alert.alert(title, message);
-    }
-}
